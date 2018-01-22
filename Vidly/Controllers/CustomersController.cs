bc11971eb5e740626f72ca94064e1c7c4ae17d12
mattskelley <matthew.skelley@tjm.com.au>
2018-01-22 16:58:46 +1000

    20180122 Commit

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -68,8 +68,8 @@ namespace Vidly.Controllers
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-            return View(customers);
+            //var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            return View();
         }
 
         public ActionResult Details(int id)
---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        //[Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        //[Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        //[Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        //[Display(Name = "Number in Stock")]
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
