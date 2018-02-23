bed1dd253afceeeaa41d4627eeed7c7c4f9911ed
mattskelley <matthew.skelley@tjm.com.au>
2018-02-23 10:34:37 +1000

    10_120 Adding Auto-completion

--- Vidly/Controllers/Api/MoviesController.cs
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -20,15 +20,37 @@ namespace Vidly.Controllers.Api
             _context = new ApplicationDbContext();
         }
 
-        // GET /api/movies
-        public IEnumerable<MovieDto> GetMovies()
+        public IEnumerable<MovieDto> GetMovies(string query = null)
         {
-            //return _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
-            return _context.Movies
+            var moviesQuery = _context.Movies
                 .Include(m => m.Genre)
+                .Where(m => m.NumberAvailable > 0);
+
+            if (!String.IsNullOrWhiteSpace(query))
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+
+            return moviesQuery
                 .ToList()
                 .Select(Mapper.Map<Movie, MovieDto>);
         }
+
+        // GET /api/movies
+        //public IEnumerable<MovieDto> GetMovies()
+        //{
+        //    //return _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+        //    return _context.Movies
+        //        .Include(m => m.Genre)
+        //        .ToList()
+        //        .Select(Mapper.Map<Movie, MovieDto>);
+        //}
+        //public IHttpActionResult GetMovie()
+        //{
+        //    var movieDtos = _context.Movies
+        //        .Include(m => m.Genre)
+        //        .ToList()
+        //        .Select(Mapper.Map<Movie, MovieDto>);
+        //    return Ok(movieDtos);
+        //}
 
         // GET /api/movies/1
         public IHttpActionResult GetMovie(int id)
